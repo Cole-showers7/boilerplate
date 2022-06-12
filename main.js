48a28df299aad0bf6152420f234d5a0ad4cda6bb
Cole-showers7 <98387486+Cole-showers7@users.noreply.github.com>
2022-06-12 14:37:06 +0100

    I 'positively' copied Keagan's work

--- main.js
+++ main.js
@@ -1,100 +1,179 @@
-  //diagonal of a square
-  function diagonalAssignment(){
-    let eachSide=9;
-        var Diagonal=eachSide * Math.sqrt(2);
-        console.log(Diagonal);      
-    
+
+  let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const buttons = document.querySelectorAll('button');
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = displayValue;
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
 }
-diagonalAssignment();
+  
+updateDisplay();
 
-
-function areaOfTriangle(){
-    var a= 5;
-    var b= 6;
-    var c=7;
-    let sP= (a + b + c)/2;
-    var Area=Math.sqrt(sP *(sP-a) *(sP-b) *(sP-c));
-    console.log(Area);
+function clickButton() {
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            if(buttons[i].classList.contains('operand')) {
+                inputOperand(buttons[i].value);
+                updateDisplay();
+            } else if(buttons[i].classList.contains('operator')) {
+                inputOperator(buttons[i].value);
+            } else if(buttons[i].classList.contains('equals')) {
+                inputEquals();
+                updateDisplay();
+            } else if(buttons[i].classList.contains('decimal')) {
+                inputDecimal(buttons[i].value);
+                updateDisplay();
+            } else if(buttons[i].classList.contains('percent')) {
+                inputPercent(displayValue);
+                updateDisplay();
+            } else if(buttons[i].classList.contains('sign')) {
+                inputSign(displayValue);
+                updateDisplay();
+            } else if(buttons[i].classList.contains('clear'))
+                clearDisplay();
+                updateDisplay();
+        }
+    )}
 }
 
-areaOfTriangle();
+clickButton();
 
-function circumference(){
-    const PI=3.142;
-    var r= 4;
-    var circumferenceOfCircle= 2 * PI * r;
-    console.log (circumferenceOfCircle);
-}
-
-circumference();
-
-function surfaceArea(){
-    const PI=3.142;
-    var r=4;
-    var surface= PI *r *r;
-    console.log(surface);
-}
-surfaceArea();
-
-function integer(a,b) {
-
-    if (a > 0 && b > 0 && a > b) {
-      console.log("The larger number is " + a );
-    } else if (a > 0 && b > 0 && a < b) {
-      console.log("The larger number is " + b);
-    } else if (a === b && a > 0 && b > 0) {
-      console.log("Both numbers are equal")
+function inputOperand(operand) {
+    if(firstOperator === null) {
+        if(displayValue === '0' || displayValue === 0) {
+            //1st click - handles first operand input
+            displayValue = operand;
+        } else if(displayValue === firstOperand) {
+            //starts new operation after inputEquals()
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
     } else {
-        console.log("Please add an integer!");
-  
+        //3rd/5th click - inputs to secondOperand
+        if(displayValue === firstOperand) {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
     }
-    
-  }
-  
-  integer(10,9);
-
-  const number = prompt("Enter any  numberof your choice: ");
-
-//check if the number is even
-if(number % 2 == 0) {
-    console.log("The number is even.");
 }
 
-// if the number is odd
-else {
-    console.log("The number is odd.");
-}
-
-//Main Js Assignment
-let computerChoice= ["rock","paper","scissors"];
-function computerPlay(computerChoice){
-
-  
-return computerChoice=computerChoice[Math.floor(Math.random() *computerChoice.length)];
-}
-
-  
-   let playerSelection = prompt("Rock, Paper or Scissors?");
-
-  function game1(playerSelection,computerChoice){
-    if (playerSelection==computerChoice){
-       console.log("It was a tie");
+function inputOperator(operator) {
+    if(firstOperator != null && secondOperator === null) {
+        //4th click - handles input of second operator
+        secondOperator = operator;
+        secondOperand = displayValue;
+        result = operate(Number(firstOperand), Number(secondOperand), firstOperator);
+        displayValue = roundAccurately(result, 15).toString();
+        firstOperand = displayValue;
+        result = null;
+    } else if(firstOperator != null && secondOperator != null) {
+        //6th click - new secondOperator
+        secondOperand = displayValue;
+        result = operate(Number(firstOperand), Number(secondOperand), secondOperator);
+        secondOperator = operator;
+        displayValue = roundAccurately(result, 15).toString();
+        firstOperand = displayValue;
+        result = null;
+    } else { 
+        //2nd click - handles first operator input
+        firstOperator = operator;
+        firstOperand = displayValue;
     }
-    if(playerSelection== "rock" && computerChoice== "paper") {
-          console.log("You loose, Paper beats rock.");
-      }
-      if(playerSelection== "paper" && computerChoice== "scissors") {
-          console.log("You loose, Scissors beats paper.");
-      }
-      if(playerSelection== "scissors" && computerChoice== "srock") {
-          console.log("You loose, Rock beats scissors.");
-      }
-      else {
-          console.log("You win.");
-      }
+}
 
-  }
-  game1(playerSelection,computerChoice);
-  console.log(playerSelection);
-  console.log(computerChoice);
- 
+function inputEquals() {
+    //hitting equals doesn't display undefined before operate()
+    if(firstOperator === null) {
+        displayValue = displayValue;
+    } else if(secondOperator != null) {
+        //handles final result
+        secondOperand = displayValue;
+        result = operate(Number(firstOperand), Number(secondOperand), secondOperator);
+        if(result === 'lmao') {
+            displayValue = 'lmao';
+        } else {
+            displayValue = roundAccurately(result, 15).toString();
+            firstOperand = displayValue;
+            secondOperand = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    } else {
+        secondOperand = displayValue;
+        result = operate(Number(firstOperand), Number(secondOperand), firstOperator);
+        if(result === 'lmao') {
+            displayValue = 'lmao';
+        } else {
+            displayValue = roundAccurately(result, 15).toString();
+            firstOperand = displayValue;
+            secondOperand = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    }
+}
+
+function inputDecimal(dot) {
+    if(displayValue === firstOperand || displayValue === secondOperand) {
+        displayValue = '0';
+        displayValue += dot;
+    } else if(!displayValue.includes(dot)) {
+        displayValue += dot;
+    } 
+}
+
+function inputPercent(num) {
+    displayValue = (num/100).toString();
+}
+
+function inputSign(num) {
+    displayValue = (num * -1).toString();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+}
+
+function inputBackspace() {
+    if(firstOperand != null) {
+        firstOperand = null;
+        updateDisplay();
+    }
+}
+
+function operate(a, b, c) {
+    if(c === '+') {
+        return a + b;
+    } else if(c === '-') {
+        return a - b;
+    } else if(c === '*') {
+        return a * b;
+    } else if(c === '/') {
+        if(b === 0) {
+            return 'lmao';
+        } else {
+        return a / b;
+        }
+    }
+}
+
+function roundAccurately(num, places) {
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
